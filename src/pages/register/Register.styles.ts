870c2ddd435767b7074a939e13f339c6ea584b4d
Eduardo Wolf <eduardogw06@gmail.com>
2022-10-17 16:47:23 -0300

    feat: register page

---
+++ src/pages/register/Register.styles.ts
@@ -0,0 +1,66 @@
+import { rgba } from "polished";
+import styled from "styled-components";
+
+const Container = styled.div`
+    padding: ${(props: any) => {
+        const sizes = props.theme.sizes;
+
+        return `${sizes.medium4} ${sizes.none} ${sizes.medium4} ${sizes.none}`;
+    }};
+
+    display: flex;
+    align-content: center;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    overflow: hidden;
+`;
+
+const FormContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    padding: ${(props: any) => {
+        const sizes = props.theme.sizes;
+
+        return `${sizes.large3} ${sizes.none}  ${sizes.large3} ${sizes.none}`;
+    }};
+
+    @media (${(props) => props.theme.media.md}) {
+        width: 300px
+    
+    };
+    
+     > div {
+        margin-bottom: 20px;
+     }
+
+     > div > div > input {
+        background: ${rgba(255, 255, 255, 0.05)};
+        color: #FFFFFF;
+        border-radius: ${props => props.theme.sizes.small1}
+     }
+
+     > div > div  {
+        &:hover {
+            > fieldset {
+                border: 1px solid #8B8BAB !important;
+            }
+            
+        }
+     }
+
+     > div > label {
+        color: #8B8BAB;
+     }
+
+     .css-1sumxir-MuiFormLabel-root-MuiInputLabel-root.Mui-focused {
+        color: #8B8BAB
+     }
+
+     .css-9ddj71-MuiInputBase-root-MuiOutlinedInput-root.Mui-focused .MuiOutlinedInput-notchedOutline {
+        border-color: #8B8BAB;
+        border-width: 2px;
+     }
+`;
+
+export { Container, FormContainer }
---
+++ src/pages/register/index.tsx
@@ -0,0 +1,58 @@
+import Button from "../../components/common/Button/Button";
+import Input from "../../components/common/Input/Input";
+import Logo from "../../components/common/Logo/Logo";
+import QuestionText from "../../components/common/QuestionText/QuestionText";
+import { Container, FormContainer } from "./Register.styles";
+
+export default function Login(): JSX.Element {
+  return (
+    <Container>
+      <Logo showImage={true} imageSide="bottom" />
+      <FormContainer>
+        <Input
+          id="name"
+          label="Nome"
+          type="text"
+          variant="outlined"
+          size="small"
+          autocomplete="off"
+        />
+        <Input
+          id="email"
+          label="E-mail"
+          type="email"
+          variant="outlined"
+          size="small"
+        />
+        <Input
+          id="password"
+          label="Senha"
+          type="password"
+          variant="outlined"
+          size="small"
+        />
+        <Input
+          id="confirmPassword"
+          label="Confirmar Senha"
+          type="password"
+          variant="outlined"
+          size="small"
+        />
+
+        <Button text="Cadastrar" size="medium" fullWidth={true} />
+        <Button
+          text="Fazer login com o Google"
+          size="medium"
+          fullWidth={true}
+          outlined={true}
+        />
+
+        <QuestionText
+          text="Já tem uma conta?"
+          linkText="Faça seu login"
+          href="/login"
+        />
+      </FormContainer>
+    </Container>
+  );
+}
