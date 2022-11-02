cea27c42d2df18f923cdf70e1aa76b978fe28592
Eduardo Wolf <eduardogw06@gmail.com>
2022-11-02 13:39:21 -0300

    wip: debug deploy

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,7 +2,7 @@ import Layout from "../components/common/Layout/Layout";
 import LayoutNoAuth from "../components/common/Layout/LayoutNoAuth";
 
 export default function App({ Component, pageProps }) {
-  console.log(Component.name);
+  console.log(Component);
 
   const pagesWithoutLayout = ["Home", "Login", "Register"];
   return pagesWithoutLayout.includes(Component.name) ? (
