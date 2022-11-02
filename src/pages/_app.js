0a8c48fc482aa343accc8f37f3c2eac8919970b7
Eduardo Wolf <eduardogw06@gmail.com>
2022-11-02 13:19:09 -0300

    fix: register page withou layout

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -4,7 +4,7 @@ import LayoutNoAuth from "../components/common/Layout/LayoutNoAuth";
 export default function App({ Component, pageProps }) {
   console.log(Component.name);
 
-  const pagesWithoutLayout = ["Home", "Login"];
+  const pagesWithoutLayout = ["Home", "Login", "Register"];
   return pagesWithoutLayout.includes(Component.name) ? (
     <LayoutNoAuth>
       <Component {...pageProps} />
