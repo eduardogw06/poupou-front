4dfd34cb4eab9fe225f90003baf8ab31a7192d20
Eduardo Wolf <eduardogw06@gmail.com>
2022-11-10 00:17:46 -0300

    refact: removing console.log

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,8 +2,8 @@ import Layout from "../components/common/Layout/Layout";
 import LayoutNoAuth from "../components/common/Layout/LayoutNoAuth";
 
 export default function App({ Component, pageProps }) {
-  console.log(Component.displayName);
   const pagesWithoutLayout = ["Home", "Login", "Register"];
+
   return pagesWithoutLayout.includes(Component.displayName) ? (
     <LayoutNoAuth>
       <Component {...pageProps} />
