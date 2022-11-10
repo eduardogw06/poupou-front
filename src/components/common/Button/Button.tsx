a25188b4bc5d9180cdc0b9c5abbd83706ad60c28
Eduardo Wolf <eduardogw06@gmail.com>
2022-11-10 00:14:20 -0300

    refact: button component

--- src/components/common/Button/Button.tsx
+++ src/components/common/Button/Button.tsx
@@ -1,23 +1,56 @@
-import { StyledButton } from "./Button.styles";
+import { CircularProgress } from "@mui/material";
+import { useTheme } from "styled-components";
+import { DefaultTheme } from "../../../types/DefaultTheme";
+import { StyledButton, ButtonContainer } from "./Button.styles";
 
 interface ButtonProps {
   text: string;
   size: "small" | "medium" | "large";
+  type?: "button" | "submit" | "reset";
   fullWidth?: boolean;
   outlined?: boolean;
+  loading?: boolean;
+  disabled?: boolean;
 }
 
 const Button = ({
+  type = "button",
   text,
   size,
   fullWidth = false,
   outlined = false,
+  loading = false,
+  disabled = false,
 }: ButtonProps): JSX.Element => {
+  const theme = useTheme() as DefaultTheme;
+  const { tertiary } = theme.colors;
+
   return (
     <>
-      <StyledButton size={size} fullWidth={fullWidth} outlined={outlined}>
-        {text}
-      </StyledButton>
+      <ButtonContainer>
+        <StyledButton
+          type={type}
+          size={size}
+          fullWidth={fullWidth}
+          outlined={outlined}
+          disabled={disabled}
+        >
+          {text}
+        </StyledButton>
+        {loading && (
+          <CircularProgress
+            size={30}
+            sx={{
+              color: tertiary,
+              position: "absolute",
+              top: "50%",
+              left: "50%",
+              marginTop: "-24px",
+              marginLeft: "-12px",
+            }}
+          />
+        )}
+      </ButtonContainer>
     </>
   );
 };
