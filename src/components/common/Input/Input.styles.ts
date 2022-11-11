8724439dd28a70cf504d452c8d037159ecc53fe0
Eduardo Wolf <eduardogw06@gmail.com>
2022-11-11 20:32:41 -0300

    refact: input styles

--- src/components/common/Input/Input.styles.ts
+++ src/components/common/Input/Input.styles.ts
@@ -1,8 +1,37 @@
 import { TextField } from "@mui/material";
+import { rgba } from "polished";
 import styled from "styled-components";
 
 const StyledTextField = styled(TextField)`
     margin-top: ${(props: any): string => props.theme.sizes.medium2};
+    
+     > div > input {
+        background: ${rgba(255, 255, 255, 0.05)};
+        color: ${(props: any): string => props.theme.colors.text}
+        border-radius: 4px;
+     }
+
+    > div  {
+        &:hover {
+            > fieldset {
+                border: 1px solid #8B8BAB !important;
+            }
+
+        }
+     }
+
+    > label {
+        color: #8B8BAB;
+    }
+
+    .css-1sumxir-MuiFormLabel-root-MuiInputLabel-root.Mui-focused {
+        color: #8B8BAB
+    }
+
+    .css-9ddj71-MuiInputBase-root-MuiOutlinedInput-root.Mui-focused .MuiOutlinedInput-notchedOutline {
+        border-color: #8B8BAB;
+        border-width: 2px;
+    }
 `
 export { StyledTextField }
 
--- src/components/pages/register/Register.styles.ts
+++ src/components/pages/register/Register.styles.ts
@@ -1,4 +1,3 @@
-import { rgba } from "polished";
 import styled from "styled-components";
 
 const Container = styled.div`
@@ -30,37 +29,9 @@ const FormContainer = styled.form`
     
     };
     
-     > div {
-        margin-bottom: 20px;
-     }
-
-     > div > div > input {
-        background: ${rgba(255, 255, 255, 0.05)};
-        color: #FFFFFF;
-        border-radius: ${props => props.theme.sizes.small1}
-     }
-
-     > div > div  {
-        &:hover {
-            > fieldset {
-                border: 1px solid #8B8BAB !important;
-            }
-            
-        }
-     }
-
-     > div > label {
-        color: #8B8BAB;
-     }
-
-     .css-1sumxir-MuiFormLabel-root-MuiInputLabel-root.Mui-focused {
-        color: #8B8BAB
-     }
-
-     .css-9ddj71-MuiInputBase-root-MuiOutlinedInput-root.Mui-focused .MuiOutlinedInput-notchedOutline {
-        border-color: #8B8BAB;
-        border-width: 2px;
-     }
+    > div {
+    margin-bottom: 20px;
+    }
 `;
 
-export { Container, FormContainer }
+export { Container, FormContainer };
