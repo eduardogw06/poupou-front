309d9ced1201b493f8339f6879b71aea2a921278
Eduardo Wolf <eduardogw06@gmail.com>
2022-11-10 21:57:25 -0300

    fix: menu item index

--- src/components/common/Sidebar/Sidebar.tsx
+++ src/components/common/Sidebar/Sidebar.tsx
@@ -20,11 +20,11 @@ import { DefaultTheme } from "../../../types/DefaultTheme";
 
 const Sidebar = (): JSX.Element => {
   const items = [
-    { description: "Meus objetivos", url: "/", icon: faPiggyBank },
-    { description: "Meus aportes", url: "/", icon: faDollar },
-    { description: "Aporte automático", url: "/", icon: faRobot },
-    { description: "Perfil", url: "/", icon: faUser },
-    { description: "Alterar senha", url: "/", icon: faKey },
+    { key: 1, description: "Meus objetivos", url: "/", icon: faPiggyBank },
+    { key: 2, description: "Meus aportes", url: "/", icon: faDollar },
+    { key: 3, description: "Aporte automático", url: "/", icon: faRobot },
+    { key: 4, description: "Perfil", url: "/", icon: faUser },
+    { key: 5, description: "Alterar senha", url: "/", icon: faKey },
   ];
 
   const theme = useTheme() as DefaultTheme;
@@ -33,7 +33,7 @@ const Sidebar = (): JSX.Element => {
   return (
     <Container>
       {items.map((item) => (
-        <Link href={item.url}>
+        <Link key={item.key} href={item.url}>
           <MenuItem>
             <IconContainer>
               <FontAwesomeIcon
