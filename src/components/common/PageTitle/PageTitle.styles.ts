45fc7bc83c1401219b86852048792a4ca7a1720a
Eduardo Wolf <eduardogw06@gmail.com>
2022-11-11 22:16:26 -0300

    refact: page title styles

--- src/components/common/PageTitle/PageTitle.styles.ts
+++ src/components/common/PageTitle/PageTitle.styles.ts
@@ -1,12 +1,16 @@
 import styled from "styled-components";
 
 const Title = styled.h1`
+    display: flex;
+    flex-direction: row;
+    width: 100%;
+    height: fit-content;
+    margin-bottom: 28px; ;
     font-family: 'Circular Std';
     font-style: normal;
     font-weight: 700;
     font-size: 32px;
     line-height: 40px;
-
     color: ${(props: any): string => props.theme.colors.text};
 `;
 
