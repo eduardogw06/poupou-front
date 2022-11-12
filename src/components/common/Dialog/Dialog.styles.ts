bccc276703bb0d1b733e191442bd2e8e90e922d8
Eduardo Wolf <eduardogw06@gmail.com>
2022-11-12 03:16:47 -0300

    refact: remove comented code

--- src/components/common/Dialog/Dialog.styles.ts
+++ src/components/common/Dialog/Dialog.styles.ts
@@ -1,17 +1,12 @@
 import Dialog from "@mui/material/Dialog";
 import styled from "styled-components";
 
-// const CustomDialog = styled(Dialog)`
-
-
-
 const CustomDialog = styled(Dialog)(({ theme }) => ({
     '& .MuiPaper-root': {
         background: `${theme === 'dark' ? '#1A1A40' : '#FFF'} !important`
     }
 
 }));
-
 
 export {
     CustomDialog
