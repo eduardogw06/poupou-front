338d249d0f7bcbdde5b0644db982905c4a73293b
Eduardo Wolf <eduardogw06@gmail.com>
2022-11-13 02:45:48 -0300

    fix: adapt component to mobile view

--- src/components/common/PageTitle/PageTitle.styles.ts
+++ src/components/common/PageTitle/PageTitle.styles.ts
@@ -3,7 +3,7 @@ import styled from "styled-components";
 const Title = styled.h1`
     display: flex;
     flex-direction: row;
-    width: 100%;
+    width: calc(100vw - 48px);
     height: fit-content;
     margin-bottom: 28px; ;
     font-family: 'Circular Std';
@@ -12,6 +12,10 @@ const Title = styled.h1`
     font-size: 32px;
     line-height: 40px;
     color: ${(props: any): string => props.theme.colors.text};
+
+    @media (${(props: any): string => props.theme.media.md}) {
+        width: 100%;
+    }
 `;
 
 export { Title }
