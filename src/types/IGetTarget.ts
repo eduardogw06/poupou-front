911ed393422a76e808f564154f40e8320bae2052
Eduardo Wolf <eduardogw06@gmail.com>
2023-04-07 00:13:58 -0300

    feat: get target service

---
+++ src/types/IGetTarget.ts
@@ -0,0 +1,9 @@
+export interface IGetTarget {
+    uuid: string;
+    description: string;
+    category_id: string;
+    user_id: string;
+    target_amount: number;
+    date_begin: Date;
+    date_end: Date;
+}
