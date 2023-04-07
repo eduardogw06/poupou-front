2a8256228b04509273a580a4db2abd5902fb22a0
Eduardo Wolf <eduardogw06@gmail.com>
2023-04-07 00:14:20 -0300

    feat: get category service

---
+++ src/services/getCategories.ts
@@ -0,0 +1,30 @@
+import { IApiResponse } from "../types/IApiResponse";
+import { IGetCategory } from "../types/IGetCategory";
+import api from "../utils/api";
+
+export const getCategorie = async (sessionToken: string): Promise<IApiResponse> => {
+    try {
+        const config = {
+            headers: { Authorization: `Bearer ${sessionToken}` }
+        };
+        const response = await api.get('/category', config);
+
+        if (Object.keys(response.data).length) {
+            return new Promise((resolve: (value: IApiResponse) => void): any =>
+                resolve({ success: true, data: response.data as IGetCategory[] }));
+        }
+
+    } catch (error) {
+        const { message } = error.response.data
+        if (message) {
+            return new Promise((resolve: (value: IApiResponse) => void): void =>
+                resolve({ success: false, message: message }))
+        }
+
+        return new Promise(
+            (resolve: (value: IApiResponse) => void): void =>
+                resolve({ success: false, message: process.env.NEXT_PUBLIC_API_DEFAULT_ERROR })
+        );
+    }
+
+}
---
+++ src/types/IGetCategory.ts
@@ -0,0 +1,6 @@
+export interface IGetCategory {
+    uuid: string;
+    description: string;
+    active: boolean;
+    icon: string;
+}
