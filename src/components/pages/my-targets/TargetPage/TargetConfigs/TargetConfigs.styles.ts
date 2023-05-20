dea77a6ddcf4ffd4fe5aae0cd02046e81874a94c
Eduardo Wolf <eduardogw06@gmail.com>
2023-05-20 13:31:43 -0300

    feat: export csv

--- src/components/pages/my-targets/TargetPage/TargetConfigs/TargetConfigs.styles.ts
+++ src/components/pages/my-targets/TargetPage/TargetConfigs/TargetConfigs.styles.ts
@@ -3,6 +3,10 @@ import styled from "styled-components";
 const Container = styled.div`
     margin-top: ${props => props.theme.sizes.medium3};
     width: calc(100vw - 104px);
+
+    @media (${(props: any): string => props.theme.media.md}) {
+        width: calc(100vw - 236px);
+    }
 `;
 
 const ButtonContainer = styled.div`
